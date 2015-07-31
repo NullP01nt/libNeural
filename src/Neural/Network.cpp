8a1e528d24b5d78c818339cb4cc3e827c933e2c5
Xander Bos <xtreme984@gmail.com>
2015-08-01 00:24:19 +0200

    Implemented backpropagation

--- src/Neural/Network.cpp
+++ src/Neural/Network.cpp
@@ -8,17 +8,17 @@ double Network::recentAverageSmoothingFactor_ = 100.0;
 Network::Network(const std::vector<unsigned> &topology) {
 	unsigned num_layers = topology.size();
 	biasnode_.setState(1.0);
-	for(unsigned l = 0; l<num_layers; l++) {
+	for(unsigned lIdx = 0; lIdx<num_layers; lIdx++) {
 		layers_.push_back(Layer());
 
-		for(unsigned n = 0; n < topology[l]; n++) {
-			layers_[l].push_back(Node());
-			std::cout << "[" << l << ":" << layers_[l][n].getID() << "]" << std::endl;
-			if(l>=1) {
-				for(unsigned np=0; np < layers_[l-1].size(); np++) {
-					layers_[l][n].addInputLink(layers_[l-1][np]);
+		for(unsigned n = 0; n < topology[lIdx]; n++) {
+			layers_[lIdx].push_back(Node());
+			std::cout << "[" << lIdx << ":" << layers_[lIdx][n].getID() << "]" << std::endl;
+			if(lIdx>=1) {
+				for(unsigned np=0; np < layers_[lIdx-1].size(); np++) {
+					layers_[lIdx][n].addInputLink(layers_[lIdx-1][np]);
 				}
-				layers_[l][n].addInputLink(biasnode_);
+				layers_[lIdx][n].addInputLink(biasnode_);
 			}
 		}
 	}
@@ -27,21 +27,45 @@ Network::Network(const std::vector<unsigned> &topology) {
 void Network::generateOutput(const std::vector<double> &inputs) {
 	assert(inputs.size()==layers_[0].size());
 
-	for(unsigned l=0; l < layers_.size(); l++) {
-		for(unsigned n = 0; n < layers_[l].size(); n++) {
-			if(l==0) {
-				layers_[l][n].setState(inputs[n]);
+	for(unsigned lIdx=0; lIdx < layers_.size(); lIdx++) {
+		for(unsigned n = 0; n < layers_[lIdx].size(); n++) {
+			if(lIdx==0) {
+				layers_[lIdx][n].setState(inputs[n]);
 			} else {
-				layers_[l][n].feedForward();
+				layers_[lIdx][n].feedForward();
 			}
+		}
+	}
+}
+
+void Network::BackProp(const std::vector<double> &outputs) {
+
+	assert(outputs.size()==layers_[layers.size()-1].size());
+
+	for(unsigned lIdx=(layers_.size()-1); lIdx>0; lIdx--) {
+		std::cout << "Iter " << lIdx << std::endl;
+		for(unsigned nIdx=0; nIdx < layers_[lIdx].size(); nIdx++) {
+			double state = layers_[lIdx][nIdx].getState();
+			double error;
+
+			if(lIdx==(layers_.size()-1)) {
+				error = state * (1.0-state) * (layers_[lIdx][nIdx]-state);
+			} else {
+				double sum = 0.0;
+				for(nIx=0; nIx < layers_[lIdx+1].size(); nIx++) {
+					sum+= layers_[lIdx+1][nIx].getIncomingWeight(layers_[lIdx][nIdx]) * layers_[lIdx+1][nIx].getError();
+				}
+				error = state * (1.0-state) * sum;
+			}
+			layers_[lIdx][nIdx].adjustWeights(error);
 		}
 	}
 }
 
 void Network::ReadResults(std::vector<double> &outputs) const {
 	outputs.clear();
-	for(unsigned o=0; o < layers_.back().size(); o++) {
-		outputs.push_back(layers_.back()[o].getState());
+	for(unsigned oIdx=0; oIdx < layers_.back().size(); oIdx++) {
+		outputs.push_back(layers_.back()[oIdx].getState());
 	}
 }
 
